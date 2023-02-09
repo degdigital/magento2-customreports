3b1b14f1411d93111880ebc63fa11465a41dba58
Jantzen Ward <jward@degdigital.com>
2023-02-09 16:46:45 -0600

    Fix error when viewing non-existent custom report

--- Controller/Adminhtml/CustomReport/Report.php
+++ Controller/Adminhtml/CustomReport/Report.php
@@ -5,47 +5,32 @@ namespace DEG\CustomReports\Controller\Adminhtml\CustomReport;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\View\Result\Page;
+use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\View\Result\PageFactory;
 
 class Report extends Action implements HttpGetActionInterface
 {
-    const ADMIN_RESOURCE = 'DEG_CustomReports::customreport_view_report';
+    public const ADMIN_RESOURCE = 'DEG_CustomReports::customreport_view_report';
 
-    /**
-     * @var \Magento\Framework\View\Result\PageFactory
-     */
-    private PageFactory $resultPageFactory;
-
-    /**
-     * @var Builder
-     */
-    private Builder $builder;
-
-    /**
-     * Report constructor.
-     *
-     * @param \Magento\Backend\App\Action\Context        $context
-     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
-     * @param Builder                                    $builder
-     */
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory,
-        Builder $builder
+        protected PageFactory $resultPageFactory,
+        protected Builder $builder
     ) {
-        $this->resultPageFactory = $resultPageFactory;
-        $this->builder = $builder;
-
         return parent::__construct($context);
     }
 
-    /**
-     * @return \Magento\Framework\View\Result\Page
-     */
-    public function execute(): Page
+    public function execute(): ResultInterface
     {
         $customReport = $this->builder->build($this->getRequest());
+        if (!$customReport->getId()) {
+            $this->messageManager->addErrorMessage(__('A custom report with the provided ID does not exist.'));
+            $redirect = $this->resultRedirectFactory->create();
+            $redirect->setPath('deg_customreports/customreport/listing');
+
+            return $redirect;
+        }
+
         $resultPage = $this->resultPageFactory->create();
         $resultPage->setActiveMenu('DEG_CustomReports::customreport');
         $resultPage->getConfig()->getTitle()->prepend($customReport->getReportName());
