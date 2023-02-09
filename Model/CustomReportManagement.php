e190eb1a13ae3577aeec6fe0e88fce381ac2f9be
Jantzen Ward <jward@degdigital.com>
2023-02-09 16:48:01 -0600

    Improve performance of both grid exports and automated exports by using the same query result for both the first page of data and the column list

--- Model/CustomReportManagement.php
+++ Model/CustomReportManagement.php
@@ -8,53 +8,34 @@ use Zend_Db_Expr;
 
 class CustomReportManagement implements CustomReportManagementInterface
 {
-    /**
-     * @var GenericReportCollectionFactory
-     */
-    protected GenericReportCollectionFactory $genericReportCollectionFactory;
+    protected array $reportCollections = [];
 
-    /**
-     * @param \DEG\CustomReports\Model\GenericReportCollectionFactory|null $genericReportCollectionFactory
-     */
     public function __construct(
-        GenericReportCollectionFactory $genericReportCollectionFactory
+        protected GenericReportCollectionFactory $genericReportCollectionFactory
     ) {
-        $this->genericReportCollectionFactory = $genericReportCollectionFactory;
     }
 
-    /**
-     * @param \DEG\CustomReports\Api\Data\CustomReportInterface $customReport
-     *
-     * @return GenericReportCollection
-     */
     public function getGenericReportCollection(CustomReportInterface $customReport): GenericReportCollection
     {
-        $genericReportCollection = $this->genericReportCollectionFactory->create();
-        $formattedSql = $this->formatSql($customReport->getReportSql());
-        $genericReportCollection->getSelect()->from(new Zend_Db_Expr('('.$formattedSql.')'));
+        if (empty($this->reportCollections[$customReport->getId()])) {
+            $genericReportCollection = $this->genericReportCollectionFactory->create();
+            $formattedSql = $this->formatSql($customReport->getReportSql());
+            $genericReportCollection->getSelect()->from(new Zend_Db_Expr('(' . $formattedSql . ')'));
 
-        return $genericReportCollection;
+            $this->reportCollections[$customReport->getId()] = $genericReportCollection;
+        }
+
+        return $this->reportCollections[$customReport->getId()];
     }
 
-    /**
-     * @param \DEG\CustomReports\Api\Data\CustomReportInterface $customReport
-     *
-     * @return string[]
-     */
     public function getColumnsList(CustomReportInterface $customReport): array
     {
         $columnsCollection = $this->getGenericReportCollection($customReport);
-        $columnsCollection->getSelect()->limitPage(1, 1);
         $firstItem = $columnsCollection->getFirstItem();
 
         return array_keys($firstItem->getData());
     }
 
-    /**
-     * @param string $rawSql
-     *
-     * @return string
-     */
     protected function formatSql(string $rawSql): string
     {
         return trim($rawSql, ';');
