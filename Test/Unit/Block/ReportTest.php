103d9a7ef1ca4b0698d9364b394c90ca1a7b66b3
Jantzen Ward <jward@degdigital.com>
2023-02-10 17:39:13 -0600

    Fix unit test

--- Test/Unit/Block/ReportTest.php
+++ Test/Unit/Block/ReportTest.php
@@ -31,7 +31,7 @@ class ReportTest extends TestCase
         $buttonList = $this->createMock(ButtonList::class);
         $this->context->method('getButtonList')->willReturn($buttonList);
         $this->data = [];
-        $this->report = new Report($this->context, $this->data, $this->data);
+        $this->report = new Report($this->context, $this->currentCustomReportRegistry, $this->data);
     }
 
     protected function tearDown(): void
